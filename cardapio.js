3b8bde28adee1d0f628913589855aa6e8df298a8
Joao Vitor <jvfernandessilva06@gmail.com>
2026-06-14 17:46:03 -0300

    feat: cria lista de itens do cardapio

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,19 @@
-console.log("Cardápio carregado com sucesso");
+const itensCardapio = [
+  {
+    nome: "X-Burguer",
+    preco: 18.90,
+    categoria: "Lanche"
+  },
+  {
+    nome: "X-Bacon",
+    preco: 21.90,
+    categoria: "Lanche"
+  },
+  {
+    nome: "Combo Família",
+    preco: 45.90,
+    categoria: "Combo"
+  }
+];
+
+console.log(itensCardapio);
